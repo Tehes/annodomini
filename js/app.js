56d6064343dee3c11670292b7a1f842408894fbc
Tino Sabetta <tino.sabetta@gmail.com>
2020-05-20 17:01:29 +0200

    started working on drag and drop events

--- js/app.js
+++ js/app.js
@@ -17,3 +17,22 @@ annoDomini = function() {
 
 	};
 }();
+
+function allowDrop(ev) {
+  ev.preventDefault();
+}
+
+function drag(ev) {
+  ev.dataTransfer.setData("text", ev.target.id = "drag");
+}
+
+function drop(ev) {
+  ev.preventDefault();
+  var data = ev.dataTransfer.getData("text");
+  if (ev.target.id === "timeline") {
+      ev.target.appendChild(document.getElementById(data));
+  }
+}
+
+var card = document.querySelector("#timeline .card");
+console.log(card.getBoundingClientRect().x);
