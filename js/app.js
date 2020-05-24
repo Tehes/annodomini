5ac9d49ae3085bf1cdac64927321681173b4a59d
Tino Sabetta <tino.sabetta@gmail.com>
2020-05-24 13:46:05 +0200

    minor tweaks in the scrollable options and moved everything in the module pattern

--- js/app.js
+++ js/app.js
@@ -6,33 +6,33 @@ annoDomini = function() {
 
     /* -------------------- Variables -------------------- */
 
-
+    var cardStack = document.querySelector("#cardStack");
+    var timeline = document.querySelector("#timeline");
 
     /* -------------------- Functions -------------------- */
 
+    new Sortable(cardStack, {
+        group: {
+            name: 'shared', // set both lists to same group
+            put: false // Do not allow items to be put into this list
+        },
+        animation: 150,
+        ghostClass: "sortable-ghost",  // Class name for the drop placeholder
+    	dragClass: "sortable-drag",  // Class name for the dragging item
+        sort: false // To disable sorting: set sort to false
+    });
 
+    new Sortable(timeline, {
+        group: 'shared',
+        animation: 150,
+        ghostClass: "sortable-ghost",  // Class name for the drop placeholder
+    	chosenClass: "sortable-chosen",  // Class name for the chosen item
+    	dragClass: "sortable-drag",  // Class name for the dragging item
+        filter: '.fixed', // 'filtered' class is not draggable
+    });
 
     /* -------------------- Public -------------------- */
     return {
 
 	};
 }();
-
-var cardStack = document.querySelector("#cardStack");
-var timeline = document.querySelector("#timeline");
-
-new Sortable(cardStack, {
-    group: 'shared', // set both lists to same group
-    animation: 150,
-    ghostClass: "sortable-ghost",  // Class name for the drop placeholder
-	dragClass: "sortable-drag"  // Class name for the dragging item
-});
-
-new Sortable(timeline, {
-    group: 'shared',
-    animation: 150,
-    ghostClass: "sortable-ghost",  // Class name for the drop placeholder
-	chosenClass: "sortable-chosen",  // Class name for the chosen item
-	dragClass: "sortable-drag",  // Class name for the dragging item
-    filter: '.fixed', // 'filtered' class is not draggable
-});
